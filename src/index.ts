8fcc8691c0f48ab2028bde722af127ebd412cc2e
Maheshr57 <mahem7080@gmail.com>
2025-03-11 20:16:03 +0530

    Add new API endpoints for random number generation, current time, environment, and number storage

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { platform } from "os";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/generate", (c) => {
+  return c.json({ random: Math.random() });
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/current", (c) => {
+  const ct = new Date().toISOString();
+  return c.json({ currentTimes: ct });
+});
+
+app.get("/environment", (c) => {
+  return c.json({ environment: process.version, platform: platform() });
+});
+
+app.get('/puppet', (c) => {
+  const qP = c.req.query();
+  return c.json(qP, 200)
+
+});
+
+
+const numbers: number[] = [];
+app.post('/numbers', async (c) => {
+  const body = await c.req.json();
+  const storedNumber = body.number;
+  numbers.push(storedNumber);
+  return c.json({ lastStoredNumber:  storedNumber}, 201);
+});
+
+app.get('/numbers', (c) => {
+  return c.json({ numbers });
+});
+
+
+console.log("Server is running on http://localhost:3000");
+
+serve(app);
